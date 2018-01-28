cb5d83bf16f8f6522a7e0286f7a8327949d1319f
Russell Ramey <doublercreative@gmail.com>
2018-01-28 10:44:23 -0600

    initial commit

---
+++ gamelist.php
@@ -0,0 +1,34 @@
+<?php
+// Get parameters 
+$filetype = $argv[1];
+$directory = $argv[2];
+
+// Scan set directory
+$files = scandir($directory);
+// Empty $gam
+$gamelist = '';
+foreach($files as $game){ 
+
+	// Remove file extension
+	$filename = str_replace($filetype, '', $game);
+
+	// Remove all meta contained in ()
+	$game_title = preg_replace('/\([^)]+\)/', '', $filename);
+
+	// Generate XML for each element
+	$gamelist_item = "<game>
+						<path>./" . $game . "</path>
+						<name>" . $game_title . "</name>
+						<image>./boxart/" . $filename . ".png</image>
+					</game> \n";
+
+	// Add xml string to main output
+	$gamelist .= $gamelist_item;
+	
+}
+
+// Out put new xml file
+file_put_contents('gamelist.xml', $gamelist);
+?>
+
+
